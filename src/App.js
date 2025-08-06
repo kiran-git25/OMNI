cd5df7d2f2669819050380a50e8fd25c67e7ea4e
kiran-git25 <kiran-sai@outlook.com>
2025-08-06 17:44:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { ChatProvider } from './contexts/ChatContext';
 import Chat from './components/Chat';
+import FileViewer from './components/FileViewer';
 import './App.css';
 
 function App() {
   return (
-    <ChatProvider>
-      <div className="app-container">
-        <Chat />
-      </div>
-    </ChatProvider>
+    <div className="app-container">
+      <main className="main-content">
+        <div className="file-viewer-container">
+          <FileViewer />
+        </div>
+        <div className="chat-container">
+          <Chat />
+        </div>
+      </main>
+    </div>
   );
 }
 
