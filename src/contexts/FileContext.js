806a3801454c8c8dfd3910fc354cfc70344959bd
kiran-git25 <kiran-sai@outlook.com>
2025-08-06 15:32:14 +0530

    Add files via upload

---
+++ src/contexts/FileContext.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from 'react';
+
+export const FileContext = createContext({
+  currentFile: null,
+  setCurrentFile: () => {}
+});
+
+export const FileProvider = ({ children }) => {
+  const [currentFile, setCurrentFile] = useState(null);
+  
+  return (
+    <FileContext.Provider value={{ currentFile, setCurrentFile }}>
+      {children}
+    </FileContext.Provider>
+  );
+};
